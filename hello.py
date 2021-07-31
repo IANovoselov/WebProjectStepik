dadf5eecdba83890958811c34999cbd040d3dd61
IANovoselov <55792442+IANovoselov@users.noreply.github.com>
2021-07-31 20:13:54 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ def app (environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    options = '\r\n'.join(environ['QUERY_STRING'].split("&"))
+    options = b'\r\n'.join(environ['QUERY_STRING'].split("&"))
     return [options]
