3588ed832a57d5b0df6a6e84862c069d178c7bbd
IANovoselov <55792442+IANovoselov@users.noreply.github.com>
2021-07-31 20:16:35 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,4 @@ def app (environ, start_response):
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
     options = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    print(options)
     return options
