bdda9acc7467aa6501a688d14881498757ab85a4
IANovoselov <55792442+IANovoselov@users.noreply.github.com>
2021-07-31 20:01:35 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
