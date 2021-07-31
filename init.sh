25e13b1f5f40afd059160692f7c0bd64f3e36130
IANovoselov <55792442+IANovoselov@users.noreply.github.com>
2021-07-31 20:17:09 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/shit.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/shit
 gunicorn -b :8080 hello:app
